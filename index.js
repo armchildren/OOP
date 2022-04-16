40956c231336b73f9dc23f317f0600bb4a570569
Raffi <69304882+armchildren@users.noreply.github.com>
2022-04-16 14:51:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+class Result {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  disance(p1, p2) {
+    return Math.sqrt(((p2.x - p1.x) ** 2) + ((p2.y - p1.y) ** 2));
+  }
+}
+const result = new Result();
+console.log(result.disance(new Roint(5, 7), new Result(11, 24)));
